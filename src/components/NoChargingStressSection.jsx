95f95941a9d16984624ac152069ce4427b0a124e
vadim obuhovschi <obuhmail@gmail.com>
2026-08-14 02:42:08 +0000

    File changes

--- src/components/NoChargingStressSection.jsx
+++ src/components/NoChargingStressSection.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image } from '@/components/ui/image';
 
-const BATTERY_IMAGE_URL = 'https://media.base44.com/images/public/6a7e5db2c2620868d1046179/926c59173_generated_image.png';
+const BATTERY_IMAGE_URL = 'https://media.base44.com/images/public/6a7e5db2c2620868d1046179/34ef9af56_Gemini_Generated_Image_gprglbgprglbgprg.jpeg';
 
 export default function NoChargingStressSection() {
   return (
