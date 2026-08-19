6c67e70dbf6f4ccbf1f3204589ec83a1c5b13e3a
vadim obuhovschi <obuhmail@gmail.com>
2026-08-19 21:49:01 +0000

    Update mobile hero image positioning

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ import FinalBookingCta from '@/components/FinalBookingCta';
 import { Image } from '@/components/ui/image';
 
 const HERO_IMAGE_URL = 'https://media.base44.com/images/public/6a7e5db2c2620868d1046179/2de60402f_generated_image.png';
-const HERO_MOBILE_IMAGE_URL = 'https://media.base44.com/images/public/6a7e5db2c2620868d1046179/628f37336_generated_image.png';
+const HERO_MOBILE_IMAGE_URL = 'https://media.base44.com/images/public/6a7e5db2c2620868d1046179/6fffda568_A6700476.jpg';
 
 function HeroField({ icon, label, children }) {
   return (
@@ -47,7 +47,7 @@ export default function Home() {
 
       {/* Hero */}
       <section className="relative h-screen min-h-[900px] md:min-h-[760px] w-full overflow-hidden">
-        <img src={HERO_MOBILE_IMAGE_URL} alt="Paradise Rentals golf cart beside a Key West beach" className="absolute inset-0 w-full h-full object-contain object-top md:hidden" />
+        <img src={HERO_MOBILE_IMAGE_URL} alt="Paradise Rentals golf cart interior with soundbar" className="absolute inset-0 w-full h-full object-contain object-bottom md:hidden" />
         <img src={HERO_IMAGE_URL} alt="Paradise Rentals white electric golf cart parked beside a Key West beach" className="absolute inset-0 w-full h-full object-cover object-right hidden md:block" />
         <div className="absolute inset-0 bg-gradient-to-r from-brand/55 via-brand/15 to-transparent md:from-brand/50 md:via-brand/10 md:to-transparent" />
         <div className="absolute inset-0 bg-gradient-to-b from-brand/40 via-transparent to-brand/55" />
