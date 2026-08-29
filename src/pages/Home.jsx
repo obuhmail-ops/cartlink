541168a1a9b00758ce54eb68f768c32c07335004
vadim obuhovschi <obuhmail@gmail.com>
2026-08-29 02:18:10 +0000

    Visual edits

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -82,7 +82,7 @@ export default function Home() {
 
             <p className="self-center md:self-start max-w-2xl leading-relaxed text-center md:text-left no-underline not-italic capitalize px-1 my-5 text-[hsl(var(--dune))] text-xl md:text-xl [font-family:'Sora',_sans-serif] font-bold">
               <span className="block md:inline [font-family:'Helvetica_Bold',_sans-serif] font-bold text-3xl">Your Cart. Your Island.</span>
-              <span className="block md:inline text-[hsl(var(--sidebar-border))] font-bold [font-family:'Helvetica_Bold',_sans-serif]">Your Adventure.</span>
+              <span className="block md:inline text-[hsl(var(--sidebar-border))] font-bold [font-family:'Helvetica_Bold',_sans-serif] text-4xl">Your Adventure.</span>
             </p>
 
             <p className="self-center md:self-start max-w-2xl leading-relaxed text-center md:text-left px-1 mt-1 text-[hsl(var(--solar))] text-lg md:text-xl [font-family:'Sora',_sans-serif] font-bold">
