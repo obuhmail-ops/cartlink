467896817a669f8b2509b8743cd4ecd32c710f43
vadim obuhovschi <obuhmail@gmail.com>
2026-08-29 01:43:55 +0000

    Change floating cart color to red

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -154,7 +154,7 @@ export default function Home() {
           src={FLOATING_CART_URL}
           alt="Paradise Rentals premium 4-passenger golf cart"
           className="w-[44rem] md:w-[57rem] lg:w-[78rem] drop-shadow-2xl"
-          style={{ mixBlendMode: 'screen' }} />
+          style={{ mixBlendMode: 'screen', filter: 'sepia(1) saturate(20) hue-rotate(-30deg) brightness(1.05) contrast(1.05)' }} />
         
       </div>
 
