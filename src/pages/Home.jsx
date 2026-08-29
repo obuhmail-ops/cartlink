6af7fe367b1b30cac861b93333676c241bfe86fe
vadim obuhovschi <obuhmail@gmail.com>
2026-08-29 00:45:08 +0000

    Update mobile hero image to Southernmost Point

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ import { Image } from '@/components/ui/image';
 
 const HERO_IMAGE_URL = 'https://media.base44.com/images/public/6a7e5db2c2620868d1046179/bfc00a16b_generated_image.png';
 const HERO_MOBILE_IMAGE_URL = 'https://media.base44.com/images/public/6a7e5db2c2620868d1046179/628f37336_generated_image.png';
-const HERO_MOBILE_PHOTO_URL = 'https://media.base44.com/images/public/6a7e5db2c2620868d1046179/4f3972395_A6700430.jpg';
+const HERO_MOBILE_PHOTO_URL = 'https://media.base44.com/images/public/6a7e5db2c2620868d1046179/41e059e25_kll.jpeg';
 const HERO_MOBILE_IMAGE_2_URL = 'https://media.base44.com/images/public/6a7e5db2c2620868d1046179/fd255c606_A6700476.jpg';
 const HERO_MOBILE_IMAGE_3_URL = 'https://media.base44.com/images/public/6a7e5db2c2620868d1046179/457bf5afe_A6700589.jpg';
 const HERO_DESKTOP_THUMB_1_URL = 'https://media.base44.com/images/public/6a7e5db2c2620868d1046179/724153794_A6700476.jpg';
@@ -127,7 +127,7 @@ export default function Home() {
 
         {/* Mobile hero photo (dimmed) */}
         <div className="md:hidden absolute inset-0 z-0">
-          <img src={HERO_MOBILE_PHOTO_URL} alt="Paradise Rentals golf carts lined up in Key West" className="w-full h-full object-cover" />
+          <img src={HERO_MOBILE_PHOTO_URL} alt="Paradise Rentals golf cart at the Southernmost Point buoy in Key West" className="w-full h-full object-cover" />
           <div className="absolute inset-0 bg-black/30" />
         </div>
 
